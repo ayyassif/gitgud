24cfcbe29f5a18edf6187711df192d84413b81d8
Javokhir Matnazarov <fracta7@gmail.com>
2025-03-21 04:33:18 +0500

    separated file and directory creation functions

--- src/init.cpp
+++ src/init.cpp
@@ -1,9 +1,10 @@
 #include "filesystem"
+#include "fstream"
 #include "gitgud.h"
 #include "iostream"
 #include "vector"
 
-int init(void) {
+int create_dirs(void) {
   int result = FAILURE;
   std::vector<std::string> directories = {
       ".git/objects", ".git/objects/info", ".git/objects/pack",
@@ -13,10 +14,39 @@ int init(void) {
     if (std::filesystem::create_directories(dir)) {
       n++;
     } else {
+      std::cerr << "Failed to create: " << dir << "\n";
       result = FAILURE;
+      break;
     }
   }
   // checks if the created directory numbers is the same as required
   result = (n == directories.size()) ? SUCCESS : FAILURE;
+  return result;
+}
+
+int create_files(void) {
+  int result = FAILURE;
+  std::vector<std::string> files = {".git/HEAD", ".git/config"};
+  int n = 0;
+  for (const auto &file : files) {
+    std::filesystem::path file_path(file);
+    std::ofstream out_file(file);
+    if (out_file) {
+      n++;
+      out_file << "" << file;
+    } else {
+      std::cerr << "Failed to create: " << file << '\n';
+      result = FAILURE;
+    }
+  }
+  result = (n == files.size()) ? SUCCESS : FAILURE;
+  return result;
+}
+
+int init(void) {
+  int result = create_dirs();
+  if (result == 1) {
+    result = create_files();
+  }
   return result;
 }
