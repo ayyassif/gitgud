968500752fc4799f356b04d7bae9dca1d9cfe868
Javokhir Matnazarov <fracta7@gmail.com>
2025-03-21 03:27:28 +0500

    added basic header file

---
+++ src/gitgud.h
@@ -0,0 +1,15 @@
+#ifndef GITGUD_H_
+#define GITGUD_H_
+
+#define SUCCESS 1
+#define FAILURE 0
+
+int init(void); // git init
+int add(char *path); // git add
+int commit(char *message); // git commit
+int push(void);
+int pull(void);
+int log(void);
+int reset(void); 
+
+#endif
